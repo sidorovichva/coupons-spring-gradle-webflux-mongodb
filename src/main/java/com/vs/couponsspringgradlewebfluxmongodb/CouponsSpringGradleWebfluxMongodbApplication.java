49ce2727d9f34c3a0a2b6313f9e5138d7a174975
sidorovichva <sidorovichva@gmail.com>
2021-08-07 09:03:32 +0300

    Initial commit

---
+++ src/main/java/com/vs/couponsspringgradlewebfluxmongodb/CouponsSpringGradleWebfluxMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.vs.couponsspringgradlewebfluxmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponsSpringGradleWebfluxMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponsSpringGradleWebfluxMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vs/couponsspringgradlewebfluxmongodb/CouponsSpringGradleWebfluxMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vs.couponsspringgradlewebfluxmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponsSpringGradleWebfluxMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
